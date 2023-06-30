5496906b3726193f31ba4c47abe9a9a5b68ad8db
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-06-30 16:32:13 +0400

    sd

--- src/404Error.js
+++ src/404Error.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
 
 export const Error = () => {
-    return (
-        <div className="flex flex-col justify center mt-52">
-        <i className="text-B-yellow text-8xl text-center bi bi-emoji-dizzy"></i>
-    <h1 className="text-B-yellow text-8xl font-bold text-center ">404 Page not found</h1>
-   </div> 
-   )
-}
+  return (
+    <div className="flex flex-col justify center mt-52">
+      <i className="text-B-yellow text-8xl text-center bi bi-emoji-dizzy"></i>
+      <h1 className="text-B-yellow text-8xl font-bold text-center ">
+        404 Page not found
+      </h1>
+    </div>
+  );
+};
