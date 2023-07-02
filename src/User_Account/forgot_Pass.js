e0ebf63be26b01aab2a2668a11ccc1cc1887f651
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-07-02 15:48:51 +0400

    Completed

--- src/User_Account/forgot_Pass.js
+++ src/User_Account/forgot_Pass.js
@@ -9,6 +9,7 @@ export const ForgotPass = () => {
       <NavBar />
       <div className="bg-white p-4 md:p-20 flex justify-center items-center">
         <div className="flex flex-wrap flex-col w-full max-w-md bg-L-black text-white p-4 md:p-8 rounded-2xl">
+          
           <h1 className="font-bold text-2xl md:text-3xl text-center mb-4">Find your Account</h1>
           <hr />
           <p className="text-center mt-4">
@@ -18,7 +19,7 @@ export const ForgotPass = () => {
             <label className="p-2">Email<span className="text-B-yellow"> *</span></label>
             <input
               type="email"
-              className="rounded-full py-2 px-4 bg-G-white text-black mb-4"
+              className="rounded-full py-2 px-4 bg-G-white text-black mb-4 w-auto"
               placeholder="Enter your Email"
             />
             <div className="flex justify-center gap-4">
