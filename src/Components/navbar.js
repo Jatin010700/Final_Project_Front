468050435113af97d6bec5678580b3a8b284ce75
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-07-05 21:52:01 +0400

    Fix Navbar Code

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -16,6 +16,19 @@ export const NavBar = () => {
     setShowMobileNav(!showMobileNav);
   };
 
+  const closeMobileNav = () => {
+    setShowMobileNav(false);
+  };
+
+  const NavButton = ({ to, text }) => (
+    <Link
+      to={to}
+      className="p-2 rounded-full font-bold hover:text-B-yellow transition ease-in-out hover:-translate-z-1 hover:scale-125"
+    >
+      {text}
+    </Link>
+  );
+
   return (
     <div className="flex flex-wrap justify-between items-center bg-white p-2 w-full sticky top-0 z-50 shadow">
       <h1 className="font-bold p-2 text-2xl">
@@ -24,44 +37,27 @@ export const NavBar = () => {
       </h1>
       <nav className="hidden md:flex md:items-center justify-center flex-wrap gap-2">
         {/* Navigation links */}
-        <Link
-          to="/"
-          className="p-2 rounded-full font-bold hover:text-B-yellow transition ease-in-out hover:-translate-z-1 hover:scale-125"
-        >
-          <span className="underline decoration-B-yellow">Home</span>
-        </Link>
-        <Link
-          to="/soon"
-          className="p-2 rounded-full font-bold hover:text-B-yellow transition ease-in-out hover:-translate-z-1 hover:scale-125"
-        >
-          About
-        </Link>
-        <Link
-          to="/soon"
-          className="p-2 rounded-full font-bold hover:text-B-yellow transition ease-in-out hover:-translate-z-1 hover:scale-125"
-        >
-          Review
-        </Link>
-        <Link
-          to="/contact"
-          className="p-2 rounded-full font-bold hover:text-B-yellow transition ease-in-out hover:-translate-z-1 hover:scale-125"
-        >
-          Contact
-        </Link>
+        <NavButton to="/" text="Home" />
+        <NavButton to="/soon" text="About" />
+        <NavButton to="/soon" text="Review" />
+        <NavButton to="/contact" text="Contact" />
         {userLogin ? (
-          <button
-            className="p-2 px-4 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
-            onClick={handleLogout}
-          >
-            LOG OUT
-          </button>
+          <>
+            <i className="text-4xl text-B-yellow hover:text-L-black transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150 bi bi-person-circle"></i>
+            <button
+              className="p-2 px-4 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
+              onClick={handleLogout}
+            >
+              LOG OUT
+            </button>
+          </>
         ) : (
           <>
             <Link
               to="/login"
               className="p-2 px-4 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
             >
-              LOG IN
+              LOGIN
             </Link>
             <Link
               to="/register"
@@ -79,77 +75,57 @@ export const NavBar = () => {
         >
           <i className="bi bi-list text-2xl"></i>
         </button>
-        <div
-          className={`absolute top-16 right-0 mt-3 mr-8 w-80 bg-white p-3 shadow-lg rounded-2xl transition duration-300 ease-in-out transform ${
-            showMobileNav ? "opacity-100 scale-100" : "opacity-0 scale-95"
-          }`}
-        >
-          <ul className="flex flex-col items-center gap-3">
-            <li>
-              <Link
-                to="/"
-                className="p-2 text-L-black transition ease-in-out hover:-translate-z-1 hover:scale-125"
-              >
-                <span className="underline decoration-B-yellow">Home</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/soon"
-                className="p-2 text-L-black transition ease-in-out hover:-translate-z-1 hover:scale-125"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/soon"
-                className="p-2 text-L-black transition ease-in-out hover:-translate-z-1 hover:scale-125"
-              >
-                Review
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                className="p-2 text-L-black transition ease-in-out hover:-translate-z-1 hover:scale-125"
-              >
-                Contact
-              </Link>
-            </li>
-            <div className="flex gap-2">
-              {userLogin ? (
-                <li>
-                  <button
-                    className="p-2 px-6 w-28 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
-                    onClick={handleLogout}
-                  >
-                    LOG OUT
-                  </button>
-                </li>
-              ) : (
-                <>
+        {/* Mobile navbar */}
+        {showMobileNav && (
+          <div className="absolute top-16 right-0 mt-3 mr-8 w-80 bg-white p-3 shadow-lg rounded-2xl transition duration-300 ease-in-out transform">
+            <ul className="flex flex-col items-center gap-3">
+              <li>
+                <NavButton to="/" text="Home" />
+              </li>
+              <li>
+                <NavButton to="/soon" text="About" />
+              </li>
+              <li>
+                <NavButton to="/soon" text="Review" />
+              </li>
+              <li>
+                <NavButton to="/contact" text="Contact" />
+              </li>
+              <div className="flex gap-2">
+                {userLogin ? (
                   <li>
-                    <Link
-                      to="/login"
-                      className="p-2 px-4 w-28 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
+                    <p>Profile</p>
+                    <button
+                      className="p-2 px-6 w-28 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
+                      onClick={handleLogout}
                     >
-                      LOG IN
-                    </Link>
+                      LOG OUT
+                    </button>
                   </li>
-                  <li>
-                    <Link
-                      to="/register"
-                      className="p-2 px-4 w-28 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
-                    >
-                      SIGN UP
-                    </Link>
-                  </li>
-                </>
-              )}
-            </div>
-          </ul>
-        </div>
+                ) : (
+                  <>
+                    <li>
+                      <Link
+                        to="/login"
+                        className="p-2 px-4 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
+                      >
+                        LOGIN
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/register"
+                        className="p-2 px-4 bg-B-yellow rounded-full font-bold hover:bg-L-black hover:text-B-yellow shadow transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-150"
+                      >
+                        SIGN UP
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </div>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
